3e12bbbbb5a058e0bc1718014a99b0816961c68e
Jedidiah Abera <yididiabera87@gmail.com>
2024-10-11 00:53:47 +0300

    Done creating: 1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''A module for a function that concatenates two strings'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''Returns the concatenation of two strings'''
+    return str1 + str2
