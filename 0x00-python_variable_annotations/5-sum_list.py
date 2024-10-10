e11990393b92dffda53af26a114b42fb3587a413
Jedidiah Abera <yididiabera87@gmail.com>
2024-10-11 00:54:03 +0300

    Done creating: 5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''A module for summing up a list of floats'''
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''Returns the sum of all floats in a list'''
+    return sum(input_list)
