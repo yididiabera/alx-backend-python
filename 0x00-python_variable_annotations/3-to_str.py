458af6286948809802cb7c395c56f388d52b9a70
Jedidiah Abera <yididiabera87@gmail.com>
2024-10-11 00:53:59 +0300

    Done creating: 3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''A module for a function that converts a float to a string'''
+
+
+def to_str(n: float) -> str:
+    '''Returns the string representation of a float'''
+    return str(n)
