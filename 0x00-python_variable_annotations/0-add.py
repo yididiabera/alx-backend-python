bed0d75fbb5dc33ef7fef66f94e4ada9475b66ca
Jedidiah Abera <yididiabera87@gmail.com>
2024-10-11 00:53:43 +0300

    Done creating: 0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''A function that returns the sum of two floats'''
+
+
+def add(a: float, b: float) -> float:
+    '''Accepts two floats and returns their sum'''
+    return a + b
