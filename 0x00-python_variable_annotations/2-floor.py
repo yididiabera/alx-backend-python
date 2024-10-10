567189935216939ab11cb335579e7ec76b0c0b03
Jedidiah Abera <yididiabera87@gmail.com>
2024-10-11 00:53:56 +0300

    Done creating: 2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''A module for a function that returns the floor of a float'''
+import math
+
+
+def floor(n: float) -> int:
+    '''Returns the floor of a float'''
+    return math.floor(n)
